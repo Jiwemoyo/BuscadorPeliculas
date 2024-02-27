3f02aeb8d2e0f203619300acf6079e21367e2c55
D0dSou1 <0DodecahedronSoul1@gmail.com>
2024-02-27 13:13:09 -0500

    Agregando img
    Agregeue los toques finales

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -1,35 +1,55 @@
 import { useState } from "react";
 
 export const BuscadorPeliculas = () => {
-
-  const urlBase= 'https://api.themoviedb.org/3/search/movie'
-  const API_KEY ='6fed1f553c85152ca941434b2802f975'
+  const urlBase = "https://api.themoviedb.org/3/search/movie";
+  const API_KEY = "6fed1f553c85152ca941434b2802f975";
 
   const [busqueda, setBusqueda] = useState("");
-  const [peliculas, setPeliculas] = useState([])
-  const handleImputchange =(e)=>{
-    setBusqueda(e.target.value)
-  }
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    fetchPeliculas()
-  }
-  const fetchPeliculas = async()=>{
+  const [peliculas, setPeliculas] = useState([]);
+  const handleInputChange = (e) => {
+    setBusqueda(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchPeliculas();
+  };
+  const fetchPeliculas = async () => {
     try {
-      const response = await fetch(`${urlBase}?query=${busqueda}&api_key=${API_KEY}`)
+      const response = await fetch(
+        `${urlBase}?query=${busqueda}&api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      setPeliculas(data.results); // Aquí deberías acceder a la propiedad "results" de la respuesta
     } catch (error) {
-      console.log(`Ha ocurrido el siguiente error: ${error}`)
+      console.log(`Ha ocurrido el siguiente error: ${error}`);
     }
-  }
+  };
   return (
     <div className="container">
       <h1 className="title">Busca Pelis</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Escribe tu pelicula aqui" value={busqueda} onChange={handleImputchange}/>
+        <input
+          type="text"
+          placeholder="Escribe tu película aquí"
+          value={busqueda}
+          onChange={handleInputChange}
+        />
         <button type="submit" className="search-button">
-          Buscar Pelicula
+          Buscar Película
         </button>
       </form>
+      <div className="movie-list">
+        {peliculas.map((pelicula) => (
+          <div key={pelicula.id} className="movie-card">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
+              alt={pelicula.original_title}
+            />
+            <h2>{pelicula.original_title}</h2>
+            <p>{pelicula.overview}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
